64efd1ed42ae322c075d6788c76c1e6bce2a50f5
Jacek Dev <dev@jacekdev.com>
2026-03-27 15:20:46 +0100

    Add unit tests for 'manipulate_dt' functions

--- tests/testthat/test-manipulate_dt.R
+++ tests/testthat/test-manipulate_dt.R
@@ -1,4 +1,4 @@
-# TODO: Add "expect_error" tests.
+# TODO: Add "expect_error" to test assertions.
 test_that("extract_dt_list_range() returns correct range without filtering", {
   test_data_list <- list(
     name1 = data.frame(col1 = c(1:5)), name2 = data.frame(col1 = c(4:10))
@@ -22,7 +22,7 @@ test_that("get_dt_range() returns correct range", {
   expect_equal(get_dt_range(data.frame(col1 = c(3:17)), "col1"), c(3, 17))
 })
 
-test_that("select_in_dt() returns correct filtered data", {
+test_that("select_in_dt() returns correct filtered data table", {
   test_df <- data.frame(col1 = letters[1:8], col2 = c(1:8))
 
   expect_equal(
@@ -30,3 +30,98 @@ test_that("select_in_dt() returns correct filtered data", {
     data.table::data.table(col1 = c("c", "d", "e", "f", "g"), col2 = c(3:7))
   )
 })
+
+test_that("extract_trends_chart_dt() returns correct data table", {
+  test_data_list <- list(
+    name1 = data.frame(col1 = c(1:5), time = c(1:5)),
+    name2 = data.frame(col1 = c(4:10), time = c(2:8))
+  )
+  test_config <- list("name_col", list("time", c(2:4)))
+
+  expected <- data.table::data.table(
+    name_col = c(rep("name1", 3), rep("name2", 3)),
+    col1 = c(2, 3, 4, 4, 5, 6),
+    time = rep(c(2, 3, 4), 2)
+  )
+
+  expect_equal(extract_trends_chart_dt(test_data_list, test_config), expected)
+})
+
+test_that("convert_dt_to_string() returns correct string", {
+  test_df <- data.frame(
+    name_col = c(rep("name1", 3), rep("name2", 3)),
+    col1 = c(2, 3, 4, 4, 5, 6),
+    time = rep(c(2, 3, 4), 2)
+  )
+  expected <- capture.output(
+    write.csv(test_df, row.names = FALSE, na = "", quote = TRUE)
+  ) %>% paste(collapse = "\n")
+
+  expect_equal(convert_dt_to_string(test_df), expected)
+})
+
+test_that("convert_dt_to_json() returns correct JSON object", {
+  test_df <- data.frame(
+    name_col = c(rep("name1", 3), rep("name2", 3)),
+    col1 = c(2, 3, 4, 4, 5, 6),
+    time = rep(c(2, 3, 4), 2)
+  )
+  expected <- jsonlite::toJSON(
+    test_df, dataframe = "rows", pretty = FALSE, auto_unbox = TRUE, na = "null"
+  )
+
+  expect_equal(convert_dt_to_json(test_df), expected)
+})
+
+test_that("source_weight() returns correct weighted data table", {
+  test_indicator_df1 <- data.table::data.table(
+    country_id = 1,
+    time_period = c(1:7),
+    value = c(1:7) * 100
+  )
+  test_weight_df1 <- data.table::data.table(
+    country_id = 1,
+    time_period = c(2:10),
+    value = c(2:10) * 10
+  )
+
+  test_indicator_df2 <- data.table::data.table(
+    country_id = 63,
+    time_period = c(1:10),
+    value = c(1:10) * 10
+  )
+  test_weight_df2 <- data.table::data.table(
+    country_id = 63,
+    time_period = c(3:8),
+    value = c(3:8) * 100
+  )
+
+  test_indicator_df_list <- list(
+    name1 = test_indicator_df1, name2 = test_indicator_df2
+  )
+  test_weight_df_list <- list(
+    name1 = test_weight_df1, name2 = test_weight_df2
+  )
+
+  expected <- list(
+    name1 = data.table::setkey(data.table::merge.data.table(
+      test_indicator_df1,
+      test_weight_df1,
+      by = c("country_id", "time_period")
+    ), NULL),
+    name2 = data.table::setkey(data.table::merge.data.table(
+      test_indicator_df2,
+      test_weight_df2,
+      by = c("country_id", "time_period")
+    ), NULL)
+  )
+  names(expected$name1) <- c("country_id", "time_period", "value", "weight")
+  names(expected$name2) <- c("country_id", "time_period", "value", "weight")
+
+  expect_equal(
+    source_weight(list(test_indicator_df_list, test_weight_df_list)),
+    expected
+  )
+})
+
+
