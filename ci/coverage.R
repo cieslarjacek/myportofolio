5085d15521b0a817a7ff5e0b54c6033b64c7b3ba
Jacek Cieslar <dev@jacekdev.com>
2026-04-28 15:11:41 +0000

    Update CI checks and add SAST check (#8)

--- ci/coverage.R
+++ ci/coverage.R
@@ -1,4 +1,3 @@
-print("Running COVERAGE check...")
 # Install and use exact "covr" version.
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes", repos = Sys.getenv("CRAN_URL"))
@@ -9,10 +8,17 @@ if (!requireNamespace("covr", quietly = TRUE)) {
     version = "3.6.5", repos = Sys.getenv("CRAN_URL")
   )
 }
+if (!requireNamespace("testthat", quietly = TRUE)) {
+  remotes::install_version(
+    "testthat",
+    version = "3.3.2", repos = Sys.getenv("CRAN_URL")
+  )
+}
 
 library(covr)
 
 # Run check.
+message("Running COVERAGE check...")
 out <- covr::package_coverage()
 actual_coverage <- covr::percent_coverage(out)
 expected_coverage <- 60 # TODO: MAKE IT 80.
@@ -20,11 +26,7 @@ expected_coverage <- 60 # TODO: MAKE IT 80.
 print(out)
 Sys.sleep(1)
 if (actual_coverage < expected_coverage) {
-  print(paste0(
-    "Coverage below threshold: ", expected_coverage, "%"
-  ))
+  message("Coverage below threshold: ", expected_coverage, "%")
   quit(status = 1)
 }
-print(paste0(
-  "Coverage above threshold: ", expected_coverage, "%"
-))
+message("Coverage above threshold: ", expected_coverage, "%")
