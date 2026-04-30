790f0a80c9468d0672828d5cd7e3a37e4eb17319
Jacek Dev <dev@jacekdev.com>
2026-04-30 13:44:59 +0200

    Move R packages installation needed in CI to Docker

--- ci/coverage.R
+++ ci/coverage.R
@@ -1,23 +1,3 @@
-# Install and use exact "covr" version.
-if (!requireNamespace("remotes", quietly = TRUE)) {
-  install.packages("remotes", repos = Sys.getenv("CRAN_URL"))
-}
-if (!requireNamespace("covr", quietly = TRUE)) {
-  remotes::install_version(
-    "covr",
-    version = "3.6.5", repos = Sys.getenv("CRAN_URL")
-  )
-}
-if (!requireNamespace("testthat", quietly = TRUE)) {
-  remotes::install_version(
-    "testthat",
-    version = "3.3.2", repos = Sys.getenv("CRAN_URL")
-  )
-}
-
-library(covr)
-
-# Run check.
 message("Running COVERAGE check...")
 out <- covr::package_coverage()
 actual_coverage <- covr::percent_coverage(out)
